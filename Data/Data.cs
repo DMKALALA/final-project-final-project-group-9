795f0369d58f6402a1d4c294051ef2a52c36d060
DMKALALA <96592192+DMKALALA@users.noreply.github.com>
2024-12-04 12:05:34 -0500

    Added the data folder with the boilerplate code.

---
+++ Data/Data.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace final_project_group_9.Data
+{
+    public class Data
+    {
+        // Class properties and methods go here
+
+        public Data()
+        {
+            // Constructor logic goes here
+        }
+
+        public void ExampleMethod()
+        {
+            // Method logic goes here
+        }
+    }
+}
