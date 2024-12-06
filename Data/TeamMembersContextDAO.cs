10dad52d54c9d58ab40f6bb76f34a46407447cc3
Pranish Adhikari <156488539+PranishAdhikari@users.noreply.github.com>
2024-12-05 23:16:12 -0500

    TeamMembersContextDAO.cs file

---
+++ Data/TeamMembersContextDAO.cs
@@ -0,0 +1,55 @@
+
+using final-project-final-project-group-9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final-project-final-project-group-9.Data
+{
+    public class TeamMembersContextDAO : ITeamMembersContextDAO
+    {
+        TeamMembersContext _context;
+
+        public TeamMembersContextDAO(TeamMembersContext context)
+        {
+            _context = context;
+        }
+
+        public int? AddInfo(TeamMember i)
+        {
+            var data = this.GetInfoById(i.Id);
+            if (data != null)
+            {
+                return null;
+            }
+            _context.TeamMembers.Add(i);
+            return _context.SaveChanges();
+        }
+
+        public List<TeamMember> GetAllInfo()
+        {
+            return _context.TeamMembers.ToList();
+        }
+
+        public TeamMember GetInfoById(int ID)
+        {
+            return _context.TeamMembers.FirstOrDefault(x => x.Id == ID);
+        }
+
+        public int? RemoveInfoById(int ID)
+        {
+            var data = this.GetInfoById(ID);
+            if (data == null)
+            {
+                return null;
+            }
+            _context.TeamMembers.Remove(data);
+            return _context.SaveChanges();
+        }
+
+        public int? UpdateInfo(TeamMember i)
+        {
+            _context.TeamMembers.Update(i);
+            return _context.SaveChanges();
+        }
+        
+    }
+}
